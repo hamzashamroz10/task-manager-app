b57f7e40a34e78448153f19df768d1116e3c2313
hamzashamroz10 <you@example.com>
2025-07-20 19:09:03 +0500

    new update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// env file load karo
+dotenv.config();
 
 export const connectDB = async () => {
-  await mongoose.connect('mongodb://hs1829a:enovy2025@ac-q5zp32y-shard-00-00.amrxo5p.mongodb.net:27017,ac-q5zp32y-shard-00-01.amrxo5p.mongodb.net:27017,ac-q5zp32y-shard-00-02.amrxo5p.mongodb.net:27017/?ssl=true&replicaSet=atlas-3e0apt-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0')
-    .then(() => console.log('DB connected'))
-    .catch((err) => console.error('DB connection error:', err));
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB connected");
+  } catch (err) {
+    console.error("DB connection error:", err);
+  }
 };
